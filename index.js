6af68be8e536dc9946986a2b702bebb971cc46af
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-03-30 21:07:53 -0300

    Exercicio 2

--- index.js
+++ index.js
@@ -19,10 +19,58 @@ const atualizarTabela = () => {
         const celulaIdade = linha.insertCell(1);
         const celulaCargo = linha.insertCell(2);
         const celulaSalario = linha.insertCell(3);
+        const celulaAcao = linha.insertCell(4);
 
         celulaNome.textContent = funcionario.nome;
         celulaIdade.textContent = funcionario.idade;
         celulaCargo.textContent = funcionario.cargo;
         celulaSalario.textContent = funcionario.salario;
+        celulaAcao.innerHTML = `
+        <button type="button" class="editarButton" onclick="editarFuncionario(${i})">Editar</button>
+        <button type="button" class="excluirButton" onclick="excluirFuncionario(${i})">Excluir</button>
+        `
     }
 };
+
+const AtualizarFuncionarioButton = (index) => {
+    if (index < 0 || index >= funcionarios.length) {
+        throw "indice invalido";
+    }
+    const nome = document.getElementById("nome").value;
+    const idade = document.getElementById("idade").value;
+    const cargo = document.getElementById("cargo").value;
+    const salario = document.getElementById("salario").value;
+
+    funcionarios[index].nome = nome;
+    funcionarios[index].idade = idade;
+    funcionarios[index].cargo = cargo;
+    funcionarios[index].salario = salario;
+    const botao = document.getElementById("buttonEditar");
+    botao.setAttribute("onclick", "cadastrarButton()");
+    botao.textContent = "Cadastrar";
+    botao.setAttribute("id", "buttonCadastrar");
+    atualizarTabela();
+}
+
+const editarFuncionario = (index) => {
+    if (index < 0 || index >= funcionarios.length) {
+        throw "indice invalido";
+    }
+    let funcionario = funcionarios[index];
+    document.getElementById("nome").value = funcionario.nome;
+    document.getElementById("idade").value = funcionario.idade;
+    document.getElementById("cargo").value = funcionario.cargo;
+    document.getElementById("salario").value = funcionario.salario;
+    const botao = document.getElementById("buttonCadastrar");
+    botao.setAttribute("onclick", `AtualizarFuncionarioButton(${index})`);
+    botao.textContent = "Atualizar funcionario";
+    botao.setAttribute("id", "buttonEditar");
+}
+
+const excluirFuncionario = (index) => {
+    if (index < 0 || index >= funcionarios.length) {
+        throw "indice invalido";
+    }
+    funcionarios.splice(index, 1);
+    atualizarTabela();
+}
